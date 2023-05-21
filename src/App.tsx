183372518d1ca3230e68d71571487c462985e5f5
Fabian Schmidt <f.schmidt3@oth-aw.de>
2023-05-21 10:09:47 +0200

    Added React Router Dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Layout from './Layout';
+import HomeScreen from './HomeScreen';
+import Dashboard from './Dashboard';
+import NoPage from './NoPage';
+
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<HomeScreen/>}/>
+          <Route path="Dashboard" element={<Dashboard/>}/>
+          <Route path="*" element={<NoPage/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    
   );
 }
 
