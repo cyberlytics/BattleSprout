5dc4af3fce4c5c93536902e036e67375eb3216f0
Fabian Klinger <f.klinger1@oth-aw.de>
2023-06-08 14:56:03 +0200

    feat: introduce PlantTile, Plant and Vector2
    Former-commit-id: 9ce99bf38c7aac8d3d283e52077b6aa6c47c079e

---
+++ sys-src/backend/src/game/Plant.ts
@@ -0,0 +1,33 @@
+
+
+
+class Plant {
+
+     length: number;
+     plantTiles: PlantTile[] = [];
+
+
+    constructor(length: number, plantTiles: PlantTile[]) {
+
+        this.length = length;
+        this.plantTiles = plantTiles;
+    }
+
+
+    public splashPlantTile(position: Vector2){
+
+        const plantTile = this.plantTiles.find(plantTile => plantTile.position.x === position.x && plantTile.position.y === position.y);
+        if(plantTile){
+            plantTile.isSplashed = true;
+        }
+    }
+
+    public isAllPlantTilesSplashed(){
+        return this.plantTiles.every(plantTile => plantTile.isSplashed);
+    }
+
+    public getPlantTile(position: Vector2){
+        return this.plantTiles.find(plantTile => plantTile.position.x === position.x && plantTile.position.y === position.y);
+    }
+
+}
