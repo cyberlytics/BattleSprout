8f607b766aad87a6a7001ba79aef01e0903c0207
Natalie Stricker <n.stricker@oth-aw.de>
2023-06-09 19:23:40 +0200

    Improved Friendlist Component
    Former-commit-id: 3e89b243e45af5b5e18111828f2840f014fba05a

--- sys-src/frontend/src/areas/mainMenu/components/FriendList.tsx
+++ sys-src/frontend/src/areas/mainMenu/components/FriendList.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useState } from 'react';
 import {
     Grid,
@@ -15,10 +14,20 @@ import {
     DialogContentText,
     DialogTitle,
     ListItemAvatar,
-    Avatar
+    Avatar,
+    Drawer,
+    Box,
+    Divider,
+    Tooltip
 } from '@mui/material';
-import Delete from '@mui/icons-material/Delete';
-import Face from '@mui/icons-material/Face';
+import { 
+    Delete,
+    Face,
+    Menu,
+    Close,
+    SportsEsports,
+    GroupAdd
+} from '@mui/icons-material';
 
 interface IFriend {
     Name: string;
@@ -27,27 +36,35 @@ interface IFriend {
 //FriendList Component
 export const FriendList = () => {
 
-    const xsValue = 12;
-    const mdValue = 6;
-    const lgValue = 4;
-
     //Maximale Anzahl an Freunden
     const MAX_flist = 10;
 
-    //Dialog der nach dem Löschen eines Freundes erscheint
-    const [open, setOpen] = React.useState(false);
+    //Ändert DialogTitel und DialogContent
+    const [DiTi, setDiTi] = useState(false);
+    const [DiCo, setDiCo] = useState(false);
 
-    const handleClickOpen = () => {
-        setOpen(true);
+    //Für das öffnen/schließen des Dialoges & des Drawer-Menu
+    const [openDialog, setDialog] = useState(false);
+    const [openDrawer, setDrawer] = useState(false);
+
+    //Öffnet Dialog mit zugehörigem Titel/Content
+    const handleDialogOpen = (Titel: any, Content: any) => {
+        setDialog(true);
+        setDiTi(Titel);
+        setDiCo(Content);
     };
 
-    const handleClose = () => {
-        setOpen(false);
+    //Schließt Dialog
+    const handleDialogClose = () => {
+        setDialog(false);
     };
 
+    //Name des neuen Freundes
     const [friend, setfriend] = useState<string>("");
+    //Freundesliste
     const [flist, setflist] = useState<IFriend[]>([]);
 
+    //setzt friend auf Textfield input
     const handleChange = (event: any): void => {
         setfriend(event.target.value);
     }
@@ -57,12 +74,16 @@ export const FriendList = () => {
         event.preventDefault();
         if(friend.length !== 0) {
             if(flist.length === MAX_flist){
-                alert("Deine Freundesliste ist voll!")
+                const Titel = "Fehlerhafte Eingabe";
+                const Content = "Deine Freundesliste ist voll!";
+                handleDialogOpen(Titel,Content);
             }
             else{
                 for (let i = 0; i < flist.length; i++) {
                     if (flist[i].Name === friend) {
-                        alert("Ihr seid schon befreundet!")
+                        const Titel = "Fehlerhafte Eingabe";
+                        const Content = "Ihr seid schon befreundet!";
+                        handleDialogOpen(Titel,Content);
                         return
                     }
                 }
@@ -70,77 +91,124 @@ export const FriendList = () => {
                 setflist([...flist,newFriend]);
             }
         } else {
-            alert("Es wurde nichts eingegeben!")
+            const Titel = "Fehlerhafte Eingabe";
+            const Content = "Es wurde nichts eingegeben!";
+            handleDialogOpen(Titel,Content);
         }
+    }
+
+    //Toggle für Side-Menu
+    const toggleDrawer = (openDrawer: any) => (event: any) => {
+        if(event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')){
+            return;
+        }
+        setDrawer(openDrawer);
     }
 
     return (
         <>
-            <Grid item xs={xsValue} md={mdValue} lg={lgValue}>
+        <Tooltip title="Freundesliste" placement='right'>
+            <IconButton
+                aria-label='open Drawer'
+                onClick={toggleDrawer(true)}>
+                <Menu/>
+            </IconButton>
+        </Tooltip>
+
+        <Drawer
+            anchor='left'
+            variant='temporary'
+            open={openDrawer}
+            onClose={toggleDrawer(false)}
+        >
+            <Box>
+                <Tooltip title="Schließen" placement='bottom'>
+                    <IconButton sx={{margin: 1, float: 'right'}}>
+                        <Close
+                            onClick={toggleDrawer(false)}
+                        />
+                    </IconButton>
+                </Tooltip>
+
                 <Grid
                     container
                     spacing={1}
                     direction={'row'}
                     alignItems={'center'}
                     style={{
-                        padding: 10,
-                        background: 'rgba(52,52,52,0.1)'
+                        margin: 1
                     }}
                 >
-                    <Grid item xs={xsValue} md={mdValue} lg={lgValue}>
+                    <Grid item>
                         <Typography 
                             variant="h6"
-                            gutterBottom
-                            >
+                            sx={{marginLeft: 3}}
+                        >
                             Du hast {flist.length}/10 Freunde!
                         </Typography>
                     </Grid>
-                    <Grid item xs={xsValue} md={mdValue} lg={lgValue}>
+                    <Grid item>
                         <TextField
                             placeholder='Name'
                             value={friend}
                             onChange={handleChange}
+                            sx={{marginLeft: 3}}
                         />
                     </Grid>
-                    <Grid item xs={xsValue} md={mdValue} lg={lgValue}>
-                        <Button
-                            onClick={AddFriend}
-                            fullWidth
-                            style={{
-                                background: '#bbd8b1',
-                                color: 'black',
-                                width: '120px',
-                                height: '50px'
-                            }}
-                            >
-                            Freund hinzufügen
-                        </Button>
+                    <Grid item>
+                        <Tooltip title="Freund hunzufügen" placement='bottom'>
+                            <IconButton
+                                onClick={AddFriend}
+                                sx={{marginLeft: 3}}
+                                >
+                                <GroupAdd/>
+                            </IconButton>
+                        </Tooltip>
                     </Grid>
                 </Grid>
-                <Grid item xs={xsValue} md={mdValue} lg={lgValue}>
-                    <List sx={{ width: '100%', maxWidth: 360, background: '#bbd8b1', padding: 1}}>
+
+                <Divider sx={{margin: 5}}/>
+
+                <Grid>
+                    <List sx={{ width: '100%', maxWidth: 600, background: '#bbd8b1', margin: 2}}>
                         {flist.map((value) => (
                             <ListItem
                                 key={value.Name}
                                 disableGutters
+                                sx={{padding: 2}}
                                 secondaryAction={
-                                    <IconButton 
-                                        aria-label="delete"
-                                        onClick={() => {
-                                            //Öffnet Info-Dialog
-                                            handleClickOpen();
-                                            //Entfernt Freund aus der Freundesliste
-                                            const complete = (FriendToDelete: string): void => {
-                                                setflist(flist.filter((friend) => {
-                                                    return friend.Name !== FriendToDelete
-                                                }))}
-                                            complete(value.Name);
-                                        }}
-                                    >
-                                        <Avatar style={{ backgroundColor: 'white'}}>
-                                            <Delete color="primary" style={{ backgroundColor: 'white'}} />
-                                        </Avatar>
-                                    </IconButton>
+                                    <>
+                                    <Tooltip title="Zum Spiel einladen" placement='bottom'>
+                                        <IconButton
+                                            aria-label="invite"
+                                        >
+                                            <Avatar style={{ backgroundColor: 'white'}}>
+                                                <SportsEsports color="primary" style={{ backgroundColor: 'white'}}></SportsEsports>
+                                            </Avatar>
+                                        </IconButton>
+                                    </Tooltip>
+                                    <Tooltip title="Freund entfernen" placement='bottom'>
+                                        <IconButton 
+                                            aria-label="delete"
+                                            onClick={() => {
+                                                //Öffnet Info-Dialog
+                                                const Titel ="Freund entfernt";
+                                                const Content ="Dein Freund wurde erfolgreich entfernt!";
+                                                handleDialogOpen(Titel, Content);
+                                                //Entfernt Freund aus der Freundesliste
+                                                const complete = (FriendToDelete: string): void => {
+                                                    setflist(flist.filter((friend) => {
+                                                        return friend.Name !== FriendToDelete
+                                                    }))}
+                                                complete(value.Name);
+                                            }}
+                                        >
+                                            <Avatar style={{ backgroundColor: 'white'}}>
+                                                <Delete color="primary" style={{ backgroundColor: 'white'}} />
+                                            </Avatar>
+                                        </IconButton>
+                                    </Tooltip>
+                                    </>
                                 }
                             >
                                 <ListItemAvatar>
@@ -153,27 +221,29 @@ export const FriendList = () => {
                         ))}
                     </List>
                 </Grid>
-            </Grid>
-            <Dialog
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="alert-dialog-title"
-                aria-describedby="alert-dialog-description"
-            >
-                <DialogTitle id="alert-dialog-title">
-                    {"Freund entfernt"}
-                </DialogTitle>
-                <DialogContent>
-                    <DialogContentText id="alert-dialog-description">
-                        Dein Freund wurde erfolgreich entfernt!
-                    </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose} autoFocus>
-                        Ok
-                    </Button>
-                </DialogActions>
-            </Dialog>
+            </Box>
+        </Drawer>
+
+        <Dialog
+            open={openDialog}
+            onClose={handleDialogClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+        >
+            <DialogTitle id="alert-dialog-title">
+                {DiTi}
+            </DialogTitle>
+            <DialogContent>
+                <DialogContentText id="alert-dialog-description">
+                    {DiCo}
+                </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleDialogClose} autoFocus>
+                    {"Ok"}
+                </Button>
+            </DialogActions>
+        </Dialog>
         </>
     );
 }
