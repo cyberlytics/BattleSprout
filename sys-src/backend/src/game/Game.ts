92cdd88c32681fe7f3c38ec52c93656449579ebe
Fabian Klinger <f.klinger1@oth-aw.de>
2023-06-10 14:25:52 +0200

    feat: introduced core gamelogic
    Former-commit-id: 30c2f6aa22e7e935a44314d1e56dee406f02e3b0

---
+++ sys-src/backend/src/game/Game.ts
@@ -0,0 +1,110 @@
+enum GameState {
+    JOINING,
+    SETUP,
+    PLAYING,
+}
+
+class Player {
+    name: string;
+    board: Board;
+    ready: boolean;
+
+    constructor(name: string) {
+        this.name = name;
+        this.board = new Board();
+        this.ready = false;
+    }
+}
+
+class Game {
+    private players: Player[];
+    private currentPlayerIndex: number;
+    private gameState: GameState;
+
+    constructor() {
+        this.players = [];
+        this.currentPlayerIndex = 0;
+        this.gameState = GameState.JOINING;
+    }
+
+    public joinGame(playerName: string): void {
+        if (this.gameState !== GameState.JOINING || this.players.length >= 2) {
+            throw new Error("Cannot join game in current state or game is full.");
+        }
+
+        this.players.push(new Player(playerName));
+
+        if (this.players.length === 2) {
+            this.gameState = GameState.SETUP;
+        }
+    }
+
+    public setPlantTile(playerName: string, plant: Plant): void {
+        if (this.gameState !== GameState.SETUP) {
+            throw new Error("Cannot set plant tile in current state.");
+        }
+
+        const player = this.getPlayerByName(playerName);
+        player.board.addPlant(plant);
+    }
+
+    public playerReady(playerName: string): void {
+        if (this.gameState !== GameState.SETUP) {
+            throw new Error("Cannot mark player ready in current state.");
+        }
+
+        const player = this.getPlayerByName(playerName);
+        player.ready = true;
+
+        if (this.players.every(p => p.ready)) {
+            this.gameState = GameState.PLAYING;
+        }
+    }
+
+    public processSplash(playerName: string, splash: Vector2): void {
+        if (this.gameState !== GameState.PLAYING) {
+            throw new Error("Cannot process splash in current state.");
+        }
+
+        const player = this.getPlayerByName(playerName);
+
+        if (player !== this.players[this.currentPlayerIndex]) {
+            throw new Error("Not your turn.");
+        }
+
+        const opponent = this.players[(this.currentPlayerIndex + 1) % 2];
+        const splashResult = opponent.board.splashTile(splash);
+
+        if (!splashResult.hit) {
+            this.changeTurn();
+        }
+
+        if (splashResult.sunk) {
+            opponent.board.isBoardEndState();
+        }
+    }
+
+    private getPlayerByIndex(playerIndex: number): Player {
+        const player = this.players[playerIndex];
+
+        if (!player) {
+            throw new Error(`Player ${playerIndex} not found.`);
+        }
+
+        return player;
+    }
+
+    private getPlayerByName(playerName: string): Player {
+        const player = this.players.find(p => p.name === playerName);
+
+        if (!player) {
+            throw new Error(`Player ${playerName} not found.`);
+        }
+
+        return player;
+    }
+
+    private changeTurn(): void {
+        this.currentPlayerIndex = (this.currentPlayerIndex + 1) % 2;
+    }
+}
--- sys-src/backend/src/game/Plant.ts
+++ sys-src/backend/src/game/Plant.ts
@@ -22,7 +22,7 @@ class Plant {
         }
     }
 
-    public isAllPlantTilesSplashed(){
+    public  isAllPlantTilesSplashed(){
         return this.plantTiles.every(plantTile => plantTile.isSplashed);
     }
 
