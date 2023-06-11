0d9052751bb1566caefd724b1deed33f1772a541
Tassilo Kasseckert <t.kasseckert@oth-aw.de>
2023-06-11 15:13:15 +0200

    Help dialog global
    Former-commit-id: 4016597aa08708e83398f276588d161af16145f3

--- sys-src/frontend/src/areas/mainMenu/MainMenu.tsx
+++ sys-src/frontend/src/areas/mainMenu/MainMenu.tsx
@@ -22,16 +22,6 @@ import { useState } from 'react';
 
 //Hauptmenü des Spiels. Von hier aus soll der Nutzer überall hinkommen.
 export const MainMenu = () => {
-    const [openHelpDialog, setOpenHelpDialog] = useState<boolean>(false);
-
-    const handleClickOpen = () => {
-        setOpenHelpDialog(true);
-    };
-
-    const handleClose = () => {
-        setOpenHelpDialog(false);
-    };
-
     const xsValue = 12;
     const mdValue = 6;
     const lgValue = 4;
@@ -69,40 +59,6 @@ export const MainMenu = () => {
                     />
                 </Grid>
             </Grid>
-
-            <Tooltip title={'Hilfe'} placement='top'>
-                <Fab
-                    color='secondary'
-                    style={{ position: 'absolute', bottom: 16, right: 16 }}
-                    onClick={handleClickOpen}
-                >
-                    <Help fontSize='large' color='primary' />
-                </Fab>
-            </Tooltip>
-
-            <Dialog open={openHelpDialog} onClose={handleClose} fullWidth>
-                {/* TODO: Help Dialog */}
-                <DialogActions
-                    style={{
-                        display: 'flex',
-                        justifyContent: 'flex-start',
-                    }}
-                >
-                    <Tooltip title={'Schließen'} placement='top'>
-                        <IconButton onClick={handleClose}>
-                            <Close />
-                        </IconButton>
-                    </Tooltip>
-                </DialogActions>
-                <DialogTitle>{'Filler: Das ist der Hilfe Dialog'}</DialogTitle>
-                <DialogContent>
-                    <DialogContentText>
-                        {
-                            'Hier wird ein Text und Video zur Hilfe im System Eingefügt'
-                        }
-                    </DialogContentText>
-                </DialogContent>
-            </Dialog>
         </>
     );
 };
