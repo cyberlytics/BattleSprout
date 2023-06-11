70aa31e69d15bec4f0b85ea42ac2c99b49bf151f
Montell Barbee <m.barbee@oth-aw.de>
2023-06-11 18:22:24 +0200

    added handleAuthenticate test
    Former-commit-id: d571b23b9333196ea03ca3920c557d2b7c99b9cb

--- sys-src/backend/tests/ConnectionHandler.test.ts
+++ sys-src/backend/tests/ConnectionHandler.test.ts
@@ -4,20 +4,50 @@ import { connectionList } from '../src/models/ConnectionModel';
 import 'jest';
 
 
-describe("handleHandshake", () => {
-    it("should push a new connection to the connectionList", () => {
+describe("Connectionhandler", () => {
+
+    test("handleHandshake should add connection to connectionlist", () => {
       const socket: Socket = {} as Socket; // Create a mock Socket object
       const initialConnectionList = [...connectionList]; // Create a copy of the original connectionList
-  
+      const initialConnectionListLength = initialConnectionList.length; // 
+
+    
+      //call function being tested
       handleHandshake(socket);
-  
+        
+      //Perform assertions
       expect(connectionList).toEqual([
         ...initialConnectionList,
         { playerID: "", connectionState: "handshake", gameID: "", socketID: socket.id },
       ]);
+      expect(connectionList.length).toBe(initialConnectionListLength + 1);
     });
-  });
 
+    test('handleAuthenticate should find correct connection in connectionlist', () => {
+        const socket : Socket = {
+            id: "socket123",
+        } as Socket; //Create a mock Socket object
+         const message = '123'; //Not necessary for now
+    
+        // Set up the connectionList with a known object
+        const initialConnectionList = [
+          { socketID: 'socket123', otherProperty: 'otherValue' },
+          { socketID: 'socket456', otherProperty: 'otherValue' },
+        ];
+        
+        // Call the function being tested
+        handleAuthenticate(socket, message);
+    
+        // Perform assertions
+        const expectedConnection = { socketID: 'socket123', otherProperty: 'otherValue' };
+        const connection = initialConnectionList.find(c => c.socketID === socket.id);
+        expect(connection).toEqual(expectedConnection);
+
+      });
+
+
+
+  });
 
 
 
