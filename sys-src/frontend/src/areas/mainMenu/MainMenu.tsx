b07d7e87afd837dbe7c3470eefaaca25b775ccca
Tassilo Kasseckert <t.kasseckert@oth-aw.de>
2023-06-11 15:39:34 +0200

    Add join Game Dialog
    Former-commit-id: 547adb97cc38ff32f4fd2ccfab88bfe4b4c6957b

--- sys-src/frontend/src/areas/mainMenu/MainMenu.tsx
+++ sys-src/frontend/src/areas/mainMenu/MainMenu.tsx
@@ -1,5 +1,4 @@
 import {
-    Fab,
     Grid,
     Typography,
     Dialog,
@@ -7,21 +6,47 @@ import {
     DialogContent,
     DialogContentText,
     DialogTitle,
+    Button,
+    TextField,
     IconButton,
     Tooltip,
 } from '@mui/material';
 import { MenuTile } from './components/MenuTile';
 import {
     AddCircleOutlined,
-    Close,
+    Delete,
     FormatListNumbered,
     GroupAdd,
-    Help,
 } from '@mui/icons-material';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 //Hauptmenü des Spiels. Von hier aus soll der Nutzer überall hinkommen.
 export const MainMenu = () => {
+    const [openJoinDialog, setOpenJoinDialog] = useState(false);
+    const [joinText, setJoinText] = useState('');
+
+    function handleOpenJoinDialog() {
+        setOpenJoinDialog(true);
+    }
+
+    function handleCloseJoinDialog() {
+        setOpenJoinDialog(false);
+        clearJoinText();
+    }
+
+    function submitJoinGame() {
+        //TODO: submit Text
+
+        handleCloseJoinDialog();
+    }
+
+    function clearJoinText() {
+        setJoinText('');
+    }
+
+    const navigate = useNavigate();
+
     const xsValue = 12;
     const mdValue = 6;
     const lgValue = 4;
@@ -39,7 +64,7 @@ export const MainMenu = () => {
                         icon={AddCircleOutlined}
                         title='Spiel erstellen'
                         content='Erstelle ein Spiel, dem ein anderer beitreten kann'
-                        link='/createGame'
+                        action={() => navigate('/createGame')}
                     />
                 </Grid>
                 <Grid item xs={xsValue} md={mdValue} lg={lgValue}>
@@ -47,7 +72,7 @@ export const MainMenu = () => {
                         icon={GroupAdd}
                         title='Spiel beitreten'
                         content='Tritt einem Spiel bei'
-                        link='/joinGame'
+                        action={() => handleOpenJoinDialog()}
                     />
                 </Grid>
                 <Grid item xs={xsValue} md={mdValue} lg={lgValue}>
@@ -55,10 +80,50 @@ export const MainMenu = () => {
                         icon={FormatListNumbered}
                         title='Rangliste'
                         content='Sehe dir deine und andere Statistiken an'
-                        link='/leaderBoard'
+                        action={() => navigate('/leaderBoard')}
                     />
                 </Grid>
             </Grid>
+
+            <Dialog
+                open={openJoinDialog}
+                onClose={handleCloseJoinDialog}
+                fullWidth
+            >
+                <DialogTitle>{'Spiel beitreten'}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        {'Geben Sie die Adresse zum Beitreten an: '}
+                    </DialogContentText>
+
+                    <TextField
+                        autoFocus
+                        margin='dense'
+                        label='Adresse'
+                        fullWidth
+                        variant='standard'
+                        value={joinText}
+                        onChange={(e) => setJoinText(e.target.value)}
+                        InputProps={{
+                            endAdornment: (
+                                <Tooltip title={'Löschen'}>
+                                    <IconButton onClick={clearJoinText}>
+                                        <Delete />
+                                    </IconButton>
+                                </Tooltip>
+                            ),
+                        }}
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCloseJoinDialog} variant='outlined'>
+                        {'Abbrechen'}
+                    </Button>
+                    <Button onClick={submitJoinGame} variant='contained'>
+                        {'Beitreten'}
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </>
     );
 };
