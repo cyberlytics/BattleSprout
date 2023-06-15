bbf73b2fc467da81461934a7829d137f73cb7c4c
Natalie Stricker <n.stricker@oth-aw.de>
2023-06-15 15:25:47 +0200

    Added Ranking.test & updated Ranking
    Former-commit-id: b532f701e46125c28412ea20bd4a46c0f2390b94

--- sys-src/frontend/src/areas/ranking/Ranking.tsx
+++ sys-src/frontend/src/areas/ranking/Ranking.tsx
@@ -14,16 +14,47 @@ interface IRank {
     Points: number;
 }
 
-//ToDo: Richtige Namen/ScorePoint Werte übergeben
-
 //Rangliste
 export const Ranking = () => {
     const [ranklist, setRanklist] = useState<IRank[]>([]);
+    //Maximale Listenlänge
+    const MAX_Length = 10;
+
+    //ToDo: Richtige Namen/ScorePoint Werte übergeben
+    const score = Math.random();
+    const Username = 'Max Mustermann';
 
     //test-button um Liste zu füllen
     const Add = (event: any): void => {
-        const newScore = {Name: 'Max',Points: 13};
-        setRanklist([...ranklist,newScore]);
+        //Wenn die Liste voll ist wird geprüft, ob der neue Score-Wert höher als der letztplazierte Score ist
+        //Wenn ja -> letztplazierter wird entfernt und der Highscore des neuen Users wird eingetragen
+        if(ranklist.length === MAX_Length){
+            var lowest = Number.POSITIVE_INFINITY;
+            var tmp;
+            for (var i=ranklist.length-1; i>=0; i--){
+                tmp = ranklist[i].Points;
+                if(tmp<lowest)lowest = tmp;
+            }
+            if(score>lowest){
+                ranklist.pop();
+                const newScore = {Name: Username ,Points: score};
+                setRanklist([...ranklist,newScore]);
+            }
+        }else{
+            //Neuer Listen-Eintrag
+            const newScore = {Name: Username,Points: score};
+            setRanklist([...ranklist,newScore]);
+        }
+    }
+
+    //Vergleicht ScorePoints und sortiert die Liste
+    ranklist.sort((a: {Points: number},b: {Points: number}) =>{
+        return compareScore(a,b);
+    });
+
+    //Score vergleich Funktion
+    function compareScore(a: {Points: number},b: {Points: number}): number {
+        return(a.Points > b.Points ? -1 : 1);
     }
 
     return (
@@ -39,29 +70,32 @@ export const Ranking = () => {
                 }}
             >
                 <Typography 
+                    data-testid='Titel'
                     variant='h3'
                     sx={{marginLeft: 3}}
                 >
-                    Rangliste!
+                    Die Top 10 Spieler!
                 </Typography>
                 <Button onClick={Add}> 
                     CLICK
                 </Button>
             </Grid>
+
             <Divider sx={{margin: 5}}/>
-            <Grid>
-                <List sx={{ width: '100%', background: '#bbd8b1'}}>
-                    {ranklist.map((value) => (
-                        <ListItem
-                            key={value.Name}
-                            disableGutters
-                            sx={{padding: 2}}
-                        >
-                            <ListItemText primary={`Name: ${value.Name} Punkte: ${value.Points}`} />
-                        </ListItem>
-                    ))}
-                </List>
-            </Grid>
+
+            <List 
+                data-testid='Ranklist' 
+                sx={{ background: 'rgba(52,52,52,0.2)'}}
+            >
+                {ranklist.map((value) => (
+                    <ListItem
+                        key={value.Name}
+                        sx={{padding: 1}}
+                    >
+                        <ListItemText primary={`Name: ${value.Name}`} secondary={`Punkte: ${value.Points}`} />
+                    </ListItem>
+                ))}
+            </List>
         </>
     );
 };
