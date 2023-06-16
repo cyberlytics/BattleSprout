300917b59528b7a062638dee9f2894e8f0e931cb
Fabian Klinger <f.klinger1@oth-aw.de>
2023-06-16 17:25:36 +0200

    feat: added plant place validation for frontend and button to control the plant placement.
    Former-commit-id: a481666302c067cdb6262f3b7843ea1c6f32d9c6

--- sys-src/frontend/src/areas/gameField/GameField.tsx
+++ sys-src/frontend/src/areas/gameField/GameField.tsx
@@ -3,49 +3,111 @@ import React, { useEffect, useRef, useState } from 'react';
 import { useLocation, useParams } from 'react-router-dom';
 import io, { Socket } from 'socket.io-client';
 import { GridComponent } from './GridComponent';
+import {PlantTile} from "../../PlantTile.";
 
 export enum CellState {
     EMPTY,
     PLANT,
+
+    SETUP,
     HIT,
     MISS,
 }
 
 const SOCKET_SERVER_URL = 'http://localhost:4000';
 
-export enum GameState {
+ enum GameState {
     connecting,
     joining,
     setup,
+
+    playing
 }
 
-export const GameField: React.FC = () => {
+export const GameField= () => {
     const params = useParams();
     const gameId = params.id;
+
+    console.log(gameId)
 
     const socketContext = useRef<Socket>(io(SOCKET_SERVER_URL));
     const [gameState, setGameState] = useState<GameState>(GameState.connecting);
     const location = useLocation();
     console.log(location.state)
-    //TODO: Größe vom Socket auslesen
+    const [plantTiles, setPlantTiles] = useState<PlantTile[]>([]);
     const [gameFieldSize, setGameFieldSize] = useState<number>(location.state);
 
     useEffect(() => {
         if (gameState === GameState.connecting) {
             socketContext.current.emit('handshake');
+
             setGameState(GameState.joining);
+            console.log('handshake')
         }
 
         if (gameState === GameState.joining) {
             socketContext.current.emit('joinGame', gameId);
+            console.log('joining')
             setGameState(GameState.setup);
         }
 
         //TODO: Event for gameFieldSize
-    }, []);
+    }, );
+
+
+    function setPlant() {
+
+        setPlantTiles([]);
+        socketContext.current.emit('setPlant', plantTiles);
+    }
+
+    function addPlantTile(plantTile : PlantTile) : boolean{
+
+        if(!canPlantTileBeAdded(plantTile))
+            return false;
+
+        setPlantTiles([...plantTiles, plantTile]);
+        return true;
+    }
+
+    function canPlantTileBeAdded(plantTile: PlantTile): boolean {
+        if(plantTiles.length === 0){
+            return true;
+        }
+
+        const isNotInStraightLine = checkForStraightLine(plantTiles, plantTile);
+        const hasAdjacentTile = checkForNeighbour(plantTiles, plantTile);
+
+        return hasAdjacentTile && isNotInStraightLine;
+    }
+
+    function checkForStraightLine(tiles: PlantTile[], targetTile: PlantTile): boolean {
+        return tiles.every(tile => {
+            return tile.position.x === targetTile.position.x
+                || tile.position.y === targetTile.position.y;
+        });
+    }
+
+    function checkForNeighbour(tiles: PlantTile[], targetTile: PlantTile): boolean {
+        return tiles.some(tile => {
+            const isNextInX = tile.position.x === targetTile.position.x + 1
+                || tile.position.x === targetTile.position.x - 1;
+            const isNextInY = tile.position.y === targetTile.position.y + 1
+                || tile.position.y === targetTile.position.y - 1;
+            return isNextInX || isNextInY;
+        });
+    }
 
     return (
         <div>
+            {gameState === GameState.setup &&
+
+                <button > Bereit! </button>
+            }
+
+            {gameState === GameState.setup &&
+                <button onClick={setPlant}> Pflanze setzen! </button>
+            }
             <Typography
                 variant='h2'
                 style={{
@@ -69,18 +131,22 @@ export const GameField: React.FC = () => {
                         <GridComponent
                             socketContext={socketContext}
                             gameFieldSize={gameFieldSize}
-                        />
-                    </div>
+                            addPlantTile={addPlantTile}
 
-                    <div>
-                        <Typography variant='h4'>
-                            Beet von Unknown_User
-                        </Typography>
-                        <GridComponent
-                            socketContext={socketContext}
-                            gameFieldSize={gameFieldSize}
                         />
                     </div>
+                    {gameState === GameState.playing &&
+                        <div>
+                            <Typography variant='h4'>
+                                Beet von Unknown_User
+                            </Typography>
+                            <GridComponent
+                                socketContext={socketContext}
+                                gameFieldSize={gameFieldSize}
+                                addPlantTile={addPlantTile}
+                            />
+                        </div>
+                    }
                 </div>
             </div>
         </div>
