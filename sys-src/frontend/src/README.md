6b50d0ddf996f2e349150e675d53ec92e7cc73bd
Rebecca Kietzer <r.kietzer@oth-aw.de>
2023-06-23 13:42:26 +0200

    Sprites
    Former-commit-id: 9e6e5267a1d0547e29d4bf90d1976f14b1cc84f2

--- sys-src/frontend/src/README.md
+++ sys-src/frontend/src/README.md
@@ -55,8 +55,10 @@ THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR I
 
 Hintergrund: Shutterstock
 Logo: designt mit Canva
+Samen: flaticon
 Wasser-Splash: PNGEgg
 Blume: IMGBIN
+Erde: bloomling
 
 ## Schlusswort
 
--- sys-src/frontend/src/areas/gameField/CellComponent.tsx
+++ sys-src/frontend/src/areas/gameField/CellComponent.tsx
@@ -1,5 +1,9 @@
 import { useEffect, useState } from 'react';
 import { CellState } from './GameField';
+import splash from '../../assets/Wasser-Splash.png';
+import seed from '../../assets/Samen.png';
+import flower from '../../assets/Blume.png';
+import earth from '../../assets/Erde.png';
 
 interface IProps {
     index: number;
@@ -11,7 +15,7 @@ const Cell = (props: IProps) => {
     const { index, cellState, onPlantPlaced } = props;
 
     const [cellOpacity, setCellOpacity] = useState(1);
-    const [cellColor, setCellColor] = useState('');
+    const [cellBackground, setCellBackground] = useState('');
 
     const handleCellHover = () => {
         setCellOpacity(0.7);
@@ -24,23 +28,23 @@ const Cell = (props: IProps) => {
     useEffect(() => {
         switch (cellState) {
             case CellState.EMPTY: {
-                setCellColor('#8f664d');
+                setCellBackground(earth);
                 break;
             }
             case CellState.SETUP: {
-                setCellColor("blue");
+                setCellBackground(seed);
                 break;
             }
             case CellState.PLANT: {
-                setCellColor('green');
+                setCellBackground(seed);
                 break;
             }
             case CellState.HIT: {
-                setCellColor('red');
+                setCellBackground(flower);
                 break;
             }
             case CellState.MISS: {
-                setCellColor('gray');
+                setCellBackground(splash);
                 break;
             }
         }
@@ -50,7 +54,8 @@ const Cell = (props: IProps) => {
         <div
             className='cell-component'
             style={{
-                backgroundColor: cellColor,
+                backgroundImage: `url(${cellBackground})`,
+                backgroundSize: 'cover',
                 width: '40px',
                 height: '40px',
                 border: '1px solid black',
